6eb8c076e7d0db5457c8cf1a411e2ebcfe00060d
ra2dd <radoslawpryw@gmail.com>
2023-11-03 15:38:21 +0100

    add coordinator class that fetches accounts and paginates movie data

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -2,23 +2,23 @@ import { Card } from './Card'
 import { FC, useEffect, useState } from 'react'
 import { Movie } from '../models/Movie'
 import * as web3 from '@solana/web3.js'
+import { MovieCoordinator } from '../utils/MovieCoordinator'
+import { Center, HStack, Button, Spacer } from '@chakra-ui/react'
 
 const MOVIE_REVIEW_PROGRAM_ID = 'CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN'
 
 export const MovieList: FC = () => {
     const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [movies, setMovies] = useState<Movie[]>([])
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
-        connection.getProgramAccounts(new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID))
-        .then(async (accounts) => {
-            const movies: Movie[] = accounts.map(({ account }) => {
-                return Movie.deserialize(account.data)
-            })
-
-            setMovies(movies)
-        })
-    }, [])
+        MovieCoordinator.fetchPage(
+            connection,
+            page,
+            10
+        ).then(setMovies)
+    }, [page])
     
     return (
         <div>
@@ -29,6 +29,18 @@ export const MovieList: FC = () => {
                     )
                 })
             }
+            <Center>
+                <HStack w='full' mt={2} mb={8} ml={4} mr={4}>
+                    {
+                        page > 1 && <Button onClick={() => setPage(page - 1)}>Previous</Button>
+                    }
+                    <Spacer />
+                    {
+                        MovieCoordinator.accounts.length > page * 2 &&
+                            <Button onClick={() => setPage(page + 1)}>Next</Button>
+                    }
+                </HStack>
+            </Center>
         </div>
     )
 }
